e7a4c466b7ceeeca329b2bb2dc25b00d69caf23e
Mark <techliujing@gmail.com>
2023-06-14 16:53:53 +0800

    使用最低版本vux

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button @click="handle">Click Me</button>
-    <label for="name">name</label>
-    <input type="text" v-model="msg">
+  <div class="container">
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+      <button @click="handle">Click Me</button>
+      <label for="name">name</label>
+      <input type="text" v-model="msg">
+    </div>
+    <group>
+      <cell v-for="item in values" :key="item" v-bind:title="item" value="切换语言"></cell>
+    </group>
   </div>
 </template>
 
 <script>
+import Group from 'vux/dist/components/group'
+import Cell from 'vux/dist/components/cell'
+
 export default {
+  components: {
+    Group,
+    Cell
+  },
   data () {
     return {
-      msg: 'Hello World!'
+      msg: 'Hello World!',
+      values: ['中文', 'English', '日本語', 'Français', 'Español']
     }
   },
   methods: {
@@ -23,6 +36,10 @@ export default {
 </script>
 
 <style>
+@import '~vux/dist/vux.css';
+.container {
+  width: 100%;
+}
 .hello {
   text-align: center;
 }
