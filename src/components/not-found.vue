277b5451858c64eae82f40f6e578ba3dd77ef60a
Mark <techliujing@gmail.com>
2023-07-19 11:51:21 +0800

    修改默认的404页面

--- src/components/not-found.vue
+++ src/components/not-found.vue
@@ -1,3 +1,12 @@
 <template>
-  <p>404 默认页面</p>
+  <div class="container">
+    <img src="../assets/logo.png" alt="">
+    <p>404 默认页面</p>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+</style>
