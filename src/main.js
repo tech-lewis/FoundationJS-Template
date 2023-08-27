e48b445f9bf9e70dad53773426cb7b7e519de804
Mark <techliujing@gmail.com>
2023-08-27 15:29:17 +0800

    完善打包，完善测试用例

--- src/main.js
+++ src/main.js
@@ -1,23 +1,2 @@
 import API from './core/Foundation'
-var app = API.create({
-    id: 'test',
-    // template
-    scope: {
-        msg: 'hello',
-        hello: 'Power by Foundation JS',
-        something: true,
-        changeMessage: function () {
-            app.scope.msg = 'clicked hello'
-            alert('click')
-        }
-    }
-})
-
 window.UIKit = API
-// class UIKit {
-//     create (options) {
-//         return API.create(options)
-//     }
-// }
-// var kit = new UIKit()
-// export default kit
