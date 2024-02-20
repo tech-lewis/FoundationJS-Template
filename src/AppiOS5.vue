93e41f2b1e891cc77d8aa6cbb010336d5405aa38
Mark <techliujing@gmail.com>
2024-02-20 11:52:33 +0800

    参考文档使用vue 0.11

--- src/AppiOS5.vue
+++ src/AppiOS5.vue
@@ -1,5 +1,10 @@
 <template>
   <div id='container'>
+    <h1>This is app component</h1>
+    <div v-component = 'user-view'></div>
+    <div v-component = 'news-view'></div>
+    <!-- main view -->
+    <!-- <div v-component="{{view}}" v-transition></div> -->
     <ul style="list-style: none;">
       <li v-repeat="items">
         <h3>{{$index}} - {{name}}</h3>
@@ -20,6 +25,11 @@ export default {
         { name: '商品3' }
       ]
     }
+  },
+  components: {
+    'news-view': require('./views/news-view.vue'),
+    'item-view': require('./views/item-view.vue'),
+    'user-view': require('./views/user-view.vue')
   }
 }
 </script>
---
+++ src/views/news-view.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    B
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
