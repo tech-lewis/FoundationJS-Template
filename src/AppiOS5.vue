a3d1bc32ffb15b05e745bdf0548a6c4854888c08
Mark <techliujing@gmail.com>
2024-03-21 12:02:15 +0800

    vue0.11常用指令

--- src/AppiOS5.vue
+++ src/AppiOS5.vue
@@ -1,19 +1,38 @@
 <template>
   <div id='container'>
     <h1>This is app component</h1>
-    <div v-component = 'user-view'></div>
-    <div v-component = 'news-view'></div>
-    <!-- main view -->
-    <!-- <div v-component="{{view}}" v-transition></div> -->
+    <div class="tab-item" v-show="tabIndex === 1">
+      <div v-component = 'user-view'></div>
+    </div>
+    <div class="tab-item" v-show="tabIndex === 2">
+      <div v-component = 'news-view'></div>
+    </div>
+    
+    <div class="footer">
+      <button v-on="click:selectTab1">User</button>
+      <button v-on="click:selectTab2">News</button>
+    </div>
   </div>
 </template>
 
 <script>
+// import Router from './router/vue-router.min.js'
 export default {
   el: '#app',
   name: 'AppComponent',
   data () {
-    return {}
+    return {
+      tabIndex: 1 // 默认显示user view
+    }
+  },
+  methods: {
+    selectTab2 () {
+      this.tabIndex = 2
+    },
+    selectTab1 (event) {
+      console.log(event)
+      this.tabIndex = 1
+    }
   },
   components: {
     'news-view': require('./views/news-view.vue'),
@@ -22,3 +41,17 @@ export default {
   }
 }
 </script>
+<style scoped>
+#container {
+  height: 50px;
+}
+.footer {
+  height: 30px;
+}
+.tab-item {
+  height:moz-calc(100% - 80px);
+  height:-webkit-calc(100% - 80px);
+  height:calc(40% - 1px);
+  overflow:scroll;
+}
+</style>
