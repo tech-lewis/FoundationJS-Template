781cf2a3e99fb763161a4e1a8ce59cacb0d13166
Mark <techliujing@gmail.com>
2024-03-31 08:51:47 +0800

    使用Vue2

--- src/main.js
+++ src/main.js
@@ -1,37 +1,23 @@
 import Vue11 from 'vue'
 import Vue from './vue2'
-import VueRouter from 'vue-router'
-import {configRouter} from './route-config'
-// import router from './router'
-// import * as echarts from 'echarts'
-// import 'keen-ui/dist/keen-ui.css'
-
-/* eslint-disable no-new */
-import { Button } from 'vux'
-// import 'styles/base.css'
-// import 'styles/index.css'
-Vue.component('v-button', Button)
-// install router
-Vue.use(VueRouter)
-// create router
-const router = new VueRouter({
-  history: false,
-  saveScrollPosition: true
-})
-// configure router
-configRouter(router)
-
 // 判断iOS版本, 兼容旧版本的系统
 const iOSVersion = navigator.appVersion || '' // str.match(/cpu iphone os (.*?) like mac os/)
 const isiOS = !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
 var appVersion = iOSVersion.match(/OS (\d+)_(\d+)_?(\d+)?/)
 appVersion = isiOS === true ? parseInt(appVersion[1], 10) : 99 // 最大值
 // alert(parseInt(appVersion[1], 10))
-if (isiOS && appVersion < 6) {
+if (isiOS && appVersion < 5) {
   var app = new Vue11(require('./AppiOS5.vue')) // .$mount('#app')
   console.log(typeof app)
 } else {
-  // 如果版本大于iOS 6.1就使用KeenUI
-  var App = Vue.extend(require('./App.vue'))
-  router.start(App, '#app')
+  // 如果版本大于iOS 6.0就使用Vue2
+  // start app
+  var instance = new Vue({
+    el: '#app',
+    data: {
+      showModal: false,
+      msg: '当前使用的Vue 2开发版本'
+    }
+  })
+  console.log(instance)
 }
