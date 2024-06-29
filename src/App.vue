d953fb4f83c6579ded0d0ac1cbbbeb59645bdc1d
Mark <1515352281@qq.com>
2024-06-30 01:09:56 +0800

    去掉CSS

--- src/App.vue
+++ src/App.vue
@@ -17,18 +17,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Helvetica, sans-serif;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  flex-direction: column;
-  background: #ccc;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-}
 </style>
