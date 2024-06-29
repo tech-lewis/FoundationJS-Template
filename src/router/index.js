1e9ca77d6a345d63da57eaf0775b0db0cdfc640f
Mark <1515352281@qq.com>
2024-06-30 01:06:00 +0800

    去掉无用路径

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from '../pages/Login'
 import home from '../pages/Home'
-import hello from '../components/Hello'
+// import hello from '../components/Hello'
 // Vue.use(Router)
 // const router = new Router({
 //   mode: 'history',
@@ -19,9 +19,6 @@ const router = new Router()
 router.map({
   '/': {
     component: login
-  },
-  '/hello': {
-    component: hello
   },
   '/app': {
     component: home
