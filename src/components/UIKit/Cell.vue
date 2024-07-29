f63b8f2d7cea06eb655ed678459c60842b403b0a
Mark <1515352281@qq.com>
2024-07-29 17:17:00 +0800

    Group Cell组件

---
+++ src/components/UIKit/Cell.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="weui_cell" v-if="!is_link">
+    <div class="weui_cell_bd weui_cell_primary">
+      <p>{{ title }}</p>
+      <inline-desc>{{ inline_desc }}</inline-desc>
+    </div>
+    <div class="weui_cell_ft">{{ value }}</div>
+  </div>
+  <a class="weui_cell" href="javascript:;" v-if="is_link" v-link="link">
+    <div class="weui_cell_bd weui_cell_primary">
+      <p>{{ title }}</p>
+      <inline-desc v-if="inline_desc">{{ inline_desc }}</inline-desc>
+    </div>
+    <div class="weui_cell_ft with_arrow">
+      {{ value }}
+    </div>
+  </a>
+</template>
+
+<script>
+const InlineDesc = require('./Inline-desc')
+module.exports = {
+  components: {
+    InlineDesc
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String
+    },
+    is_link: {
+      type: Boolean,
+      default: false
+    },
+    link: {
+      type: String
+    },
+    inline_desc: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style></style>
