a8ee09f33f47bf94b41775eeec91bb86c4b22d77
Mark <1515352281@qq.com>
2024-08-08 15:14:21 +0800

    typo adjust

--- src/components/UIKit/Boolean.vue
+++ src/components/UIKit/Boolean.vue
@@ -1,38 +1,36 @@
 <template>
-	<div class="weui_cell weui_cell_switch">
-	    <div class="weui_cell_hd weui_cell_primary">{{title}}</div>
-	    <div class="weui_cell_ft">
-	        <input class="weui_switch" type="checkbox" :disabled="disabled" v-model="value"/>
-	    </div>
-	</div>
+  <div class="weui_cell weui_cell_switch">
+    <div class="weui_cell_hd weui_cell_primary">{{ title }}</div>
+    <div class="weui_cell_ft">
+      <input class="weui_switch" type="checkbox" :disabled="disabled" v-model="value" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	props: {
-		title: {
-			type: String,
-			required: true
-		},
-		disabled: {
-			type: Boolean,
-			default: false,
-		},
-		value: {
-			type: Boolean
-		}
-	},
-	ready: function(){
-		console.log('ready');
-	},
-	watch :{
-		value: function(newVal){
-			console.log(newVal)
-		}
-	}
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    value: {
+      type: Boolean
+    }
+  },
+  ready: function () {
+    console.log('ready');
+  },
+  watch: {
+    value: function (newVal) {
+      console.log(newVal)
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
