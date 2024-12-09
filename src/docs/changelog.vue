95525f0fe740f823f674e0128987f261a3725785
Mark <1515352281@qq.com>
2024-12-10 01:33:35 +0800

    feat: 小日记

---
+++ src/docs/changelog.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>Hello  梦瑶</h1>
+
+    <selector description="love story" selected="2024-03-02" :readonly=true title="第一天聊天"></selector>
+    <selector description="love story" selected="2024-04-04" :readonly=true title="机场通宵"></selector>
+    <selector description="love story" selected="2024-04-05" :readonly=true title="七宝见面"></selector>
+    <selector description="love story" selected="2024-04-05" :readonly=true title="第一次去外滩"></selector>
+    <selector description="love story" selected="2024-04-06" :readonly=true title="第一次去东方明珠"></selector>
+    <selector description="love story" selected="2024-04-06" :readonly=true title="第一次梦瑶带去武康大楼"></selector>
+    <selector description="love story" selected="2024-04-05" :readonly=true title="梦瑶请客吃米线"></selector>
+    <selector description="love story" selected="2024-04-06" :readonly=true title="梦瑶请客吃先求半步颠"></selector>
+    <selector description="love story" selected="2024-05-02" :readonly=true title="去上海的路上掉了眼镜"></selector>
+    <selector description="love story" selected="2024-05-03" :readonly=true title="去看了外滩金融中心"></selector>
+    <selector description="love story" selected="2024-05-04" :readonly=true title="第一次去秀景苑~"></selector>
+    <selector description="love story" selected="2024-05-05" :readonly=true title="梦瑶请我吃早餐，吃小杨生煎还帮我擦碗碗"></selector>
+    <selector description="love story" selected="2024-05-05" :readonly=true title="第一次看话剧，‘贼’想得到你有梦瑶陪伴"></selector>
+    <selector description="love story" selected="2024-05-05" :readonly=true title="第一次送我到虹桥火车站通宵火车赶去上班"></selector>
+    <selector description="love story" selected="2024-05-12" :readonly=true title="第一次给梦瑶过节，送了巧克力和奶茶和礼物袋子"></selector>
+    <selector description="love story" selected="2024-05-20" :readonly=true title="第二次和梦瑶表白，给梦瑶送Switch了"></selector>
+    <selector description="love story" selected="2024-06-09" :readonly=true title="第一次看电影，梦瑶请我在虹桥天地看了走走停停"></selector>
+    <selector description="love story" selected="2024-06-09" :readonly=true title="第三次和梦瑶表白，带戒指"></selector>
+    <selector description="love story" selected="2024-06-10" :readonly=true title="端午节那种梦瑶的粽子早起赶火车"></selector>
+    <selector description="love story" selected="2024-06至07" :readonly=true title="梦瑶陪我看了许许多多的电影"></selector>
+    <selector description="love story" selected="2024-08-01" :readonly=true title="梦瑶带我看了好多东西，你的百褶裙好好看"></selector>
+    <selector description="love story" selected="2024-08-01" :readonly=true title="梦瑶来回几小时送我去南站"></selector>
+    <selector description="love story" selected="2024-08-02" :readonly=true title="2024年回家看看装修的婚房"></selector>
+    <selector description="love story" selected="2024-09-15" :readonly=true title="去上海，和梦瑶经历了2024上海的最强台风天"></selector>
+    <selector description="love story" selected="2024-09-15" :readonly=true title="梦瑶请我吃海底捞，很好吃的。真可爱，还等我好久的"></selector>
+    <selector description="love story" selected="2024-09-16" :readonly=true title="买蛋糕了和梦瑶过生日"></selector>
+    <selector description="love story" selected="2024-09-17" :readonly=true title="第一次梦瑶陪我吃烤鱼哦，真的好好吃。可惜油渍滴到她白色衣服上了"></selector>
+    <selector description="love story" selected="2024-09-18" :readonly=true title="记得要给梦瑶穿上婚纱哦"></selector>
+    <selector description="love story" selected="2024-10-01" :readonly=true title="没赶上火车，第二天坐G998"></selector>
+    <selector description="love story" selected="2024-10-02" :readonly=true title="第一次梦瑶独自来火车站，大晚上梦瑶接我去吃串串"></selector>
+    <selector description="love story" selected="2024-10-04" :readonly=true title="离开虹桥火车站，掉了一瓶可口可乐"></selector>
+    <selector description="love story" selected="2024-10-04" :readonly=true title="梦瑶可可爱爱的和我在17号线紧张的要命，终于改签了浦东飞机"></selector>
+    <selector description="love story" selected="2024-10-29" :readonly=true title="梦瑶找了很久行李，从宝安机场一起回家"></selector>
+    <selector description="love story" selected="2024-10-30" :readonly=true title="从机场回家咯"></selector>
+    <selector description="love story" selected="2024-10-30" :readonly=true title="梦瑶一起去爬山"></selector>
+    <selector description="love story" selected="2024-11-01" :readonly=true title="梦瑶请我吃烧烤到凌晨， 谢谢你"></selector>
+    <selector description="love story" selected="2024-11-01" :readonly=true title="梦瑶晚上等我下班，在地铁站坐了很久~辛苦小可爱了"></selector>
+    <selector description="love story" selected="2024-11-02" :readonly=true title="陪我去姐姐家，还买了好多礼物。也给我买了礼物"></selector>
+    <selector description="love story" selected="2024-11-03" :readonly=true title="梦瑶陪伴我逛了小区周围，还吃了狠辣的探鱼、对梦瑶要好一点"></selector>
+
+
+
+  </div>
+  </template>
+  
+  <script>
+  import Selector from '../components/UIKit/Select'
+  export default {
+    name: 'changelog',
+    components: {
+      Selector
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
