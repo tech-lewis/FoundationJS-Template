bcb3d395bfc76090c66faf4066f2b4091ec95989
Mark <mark@MarkdeMacBook-Air.local>
2025-06-08 23:05:54 +0800

    feat: 完成登录模块

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,16 +1,65 @@
+<script>
+import { ref } from 'vue'
+export default {
+  setup () {
+    const isLogin = ref(false)
+    const list = ref([
+      {text: '小梦瑶', age: 1996, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 1997, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 1998, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 1999, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2000, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2001, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2002, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2003, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2004, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2005, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2006, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2007, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2008, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2009, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2010, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2011, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2012, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2013, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2014, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2015, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2016, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2017, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2018, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2019, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2020, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2021, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2022, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2023, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2024, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2025, label: '没有小蛮腰'},
+      {text: '小梦瑶', age: 2026, label: '没有小蛮腰'},
+    ])
+
+    return {
+      isLogin,
+      list,
+      checkLogin: function () {
+        
+      }
+    }
+  }
+};
+</script>
+
+
 <template>
   <div class="home">
-      home
-      <van-button>去登录</van-button>
+    <van-cell-group class="list">
+      <van-cell title="单元格" value="内容" />
+      <van-cell v-for="(item, index) in list" :key="index" :title="item.text" :value="item.age" :label="item.label" @click="checkLogin" />
+    </van-cell-group>
   </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
 <style>
-
+.list {
+  width: 100vw;
+}
 </style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,15 +1,134 @@
 <template>
   <div class="login">
-      Login
+    <van-nav-bar class="nav" title="登录"></van-nav-bar>
+    <van-form @failed="onFailed" @submit="onSubmit">
+      <van-cell-group inset>
+        <!-- 通过 pattern 进行正则校验 -->
+        <van-field
+          v-model="userNo"
+          name="userNo"
+          left-icon="manager"
+          placeholder="请输入6位账号"
+          :rules="[{ pattern, message: '请输入正确的账号' }]"
+        />
+        <!-- 通过 validator 进行函数校验 -->
+        <van-field
+          v-model="phone"
+          name="phone"
+          placeholder="请输入您的手机号"
+          left-icon="phone"
+          :rules="[{ validator, message: '请输入正确内容' }]"
+        />
+        <!-- 通过 validator 返回错误提示 -->
+        <van-field
+          v-model="phone"
+          name="phone"
+          placeholder="校验函数返回错误提示"
+          left-icon="phone-o"
+          :rules="[{ validator: validatorMessage }]"
+        />
+        <!-- 通过 validator 进行异步函数校验 -->
+        <van-field
+          v-model="password"
+          name="password"
+          placeholder="异步校验您的密码"
+          left-icon="lock"
+          type="password"
+          :rules="[{ validator: asyncValidator, message: '请输入正确内容' }]"
+        />
+      </van-cell-group>
+      <div style="margin: 16px;">
+        <van-button round block type="primary" native-type="submit">Orz 我要登录</van-button>
+      </div>
+    </van-form>
+    {{password}}
   </div>
 </template>
 
 <script>
-export default {
+import { Toast } from "vant";
+import { ref, reactive, toRefs } from 'vue';
+function useSubmit () {
+  const onSubmit = async () => {}
 
+  return {
+    onSubmit // 封装的hook
+  }
 }
+
+export default {
+  setup() {
+    const value1 = ref("");
+    const value2 = ref("");
+    const value3 = ref("abc");
+    const value4 = ref("");
+    const pattern = /\d{6}/;
+
+    // 校验函数返回 true 表示校验通过，false 表示不通过
+    const validator = val => /1\d{10}/.test(val);
+
+    // 校验函数可以直接返回一段错误提示
+    const validatorMessage = val => `${val} 不合法，请重新输入`;
+
+    // 校验函数可以返回 Promise，实现异步校验
+    const asyncValidator = val =>
+      new Promise(resolve => {
+        Toast.loading("验证中...");
+
+        setTimeout(() => {
+          Toast.clear();
+          resolve(val === "1234");
+        }, 1000);
+      });
+
+    const onFailed = errorInfo => {
+      console.log("failed", errorInfo);
+    };
+
+
+    const user = reactive({
+      userNo: '',
+      phone: '',
+      password: ''
+    });
+    return {
+      value1,
+      value2,
+      value3,
+      value4,
+      pattern,
+      onFailed,
+      validator,
+      asyncValidator,
+      ...toRefs(user),
+      ...useSubmit(user),
+      onSubmit: (values) => {
+        console.log('submit', values);
+        // 模拟登录
+        Toast.loading("_(:з」∠)_登录...");
+        setTimeout(() => {
+          Toast.clear();
+        }, 1000);
+      }
+    };
+  }
+};
 </script>
 
 <style>
+.login {
+  width: 100%;
+}
+.nav {
+  height: 44px;
+  vertical-align: middle;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  background-color: #3296fa;
+}
 
+.nav .van-nav-bar__title {
+  color: #fff;
+}
 </style>
