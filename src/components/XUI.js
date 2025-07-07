139e57de70ed0e2c838cc74341f67602571aadab
刘竞 <jin.liu@xquant.com>
2025-07-07 15:48:46 +0800

    feat: 封装第二个组件并导出全部

---
+++ src/components/XUI.js
@@ -0,0 +1,85 @@
+import Radio from './XUI/Radio/index.vue'
+import Group from './XUI/Group/index.vue'
+import DevTip from './XUI/Dev-tip/index.vue'
+import Input from './XUI/Input/index.vue'
+import Number from './XUI/Number/index.vue'
+import Cell from './XUI/Cell/index.vue'
+import Checklist from './XUI/Checklist/index.vue'
+import Date from './XUI/Date/index.vue'
+import Switch from './XUI/Switch/index.vue'
+import Textarea from './XUI/Textarea/index.vue'
+import GroupTitle from './XUI/Group-title/index.vue'
+import Box from './XUI/Box/index.vue'
+import Tip from './XUI/Tip/index.vue'
+import Selector from './XUI/Select/index.vue'
+import XButton from './XUI/Button/index.vue'
+import Flexbox from './XUI/Flexbox/index.vue'
+import FlexboxItem from './XUI/Flexbox-item/index.vue'
+import Tab from './XUI/Tab/index.vue'
+import TabItem from './XUI/Tab-item/index.vue'
+import Swiper from './XUI/Swiper/index.vue'
+import SwiperItem from './XUI/Swiper/Swiper-item/index.vue'
+import Sticky from './XUI/sticky/index.vue'
+import Picker from './XUI/picker/index.vue'
+import Datetime from './XUI/datetime/index.vue'
+import Popup from './XUI/popup/index.vue'
+import Range from './XUI/range/index.vue'
+import Actionsheet from './XUI/Actionsheet/index.vue'
+import Clocker from './XUI/clocker/index.vue'
+import Rater from './XUI/rater/index.vue'
+import PopupPicker from './XUI/Popup-picker/index.vue'
+import Address from './XUI/address/index.vue'
+import Toast from './XUI/toast/index.vue'
+import Loading from './XUI/Loading/index.vue'
+import Alert from './XUI/Alert/index.vue'
+import Confirm from './XUI/Confirm/index.vue'
+import Progress from './XUI/progress/index.vue'
+import XImg from './XUI/X-img/index.vue'
+import Spinner from './XUI/Spinner/index.vue'
+import Calendar from './XUI/calendar/index.vue'
+import Icon from './XUI/Icon/index.vue'
+
+const xui = {
+  Radio,
+  Group,
+  Date,
+  DevTip,
+  Input,
+  GroupTitle,
+  Number,
+  Checklist,
+  Switch,
+  Box,
+  Tip,
+  Selector,
+  Cell,
+  XButton,
+  Textarea,
+  Flexbox,
+  FlexboxItem,
+  Tab,
+  TabItem,
+  Swiper,
+  SwiperItem,
+  Sticky,
+  Picker,
+  Datetime,
+  Popup,
+  Range,
+  Actionsheet,
+  Clocker,
+  Rater,
+  PopupPicker,
+  Address,
+  Toast,
+  Loading,
+  Alert,
+  Confirm,
+  Progress,
+  XImg,
+  Spinner,
+  Calendar,
+  Icon
+}
+
+export default xui
